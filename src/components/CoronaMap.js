85fd623776b90ccd7826f28030938b9faa341627
Pulak Chakraborty <pulakchakraborty.contact@gmail.com>
2020-05-06 03:05:05 +0200

    data driven styling - change the circle radius depending on the amount of corona cases

--- src/components/CoronaMap.js
+++ src/components/CoronaMap.js
@@ -73,9 +73,38 @@ const CoronaMap = () => {
                     // paint properties
                     paint: {
                         "circle-opacity": 0.75,
-                        "circle-stroke-width": 1,
-                        "circle-radius": 4,
-                        "circle-color": "#FF4500"
+                        "circle-stroke-width": [
+                            "interpolate",
+                            ["linear"],
+                            ["get", "cases"],
+                            1, 1,
+                            200000, 1.8,
+                        ],
+                        "circle-radius": [
+                            "interpolate",
+                            ["linear"],
+                            ["get", "cases"],
+                            1, 4,
+                            1000, 8,
+                            4000, 10,
+                            8000, 14,
+                            12000, 18,
+                            100000, 40,
+                            200000, 50
+                        ],
+                        "circle-color": [
+                            "interpolate",
+                            ["linear"],
+                            ["get", "cases"],
+                            1, '#ffffcc',
+                            5000, '#ffeda0',
+                            10000, '#fed976',
+                            25000, '#feb24c',
+                            50000, '#fd8d3c',
+                            75000, '#fc4e2a',
+                            100000, '#e31a1c',
+                            200000, '#b10026'
+                        ]
                     }
                 });
             });
