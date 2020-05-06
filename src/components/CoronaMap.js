7fb26de89dfa5ba9330613caa43781ee595596aa
Pulak Chakraborty <pulakchakraborty.contact@gmail.com>
2020-05-06 02:36:41 +0200

    Add covid-19 data from API - use the data from api as input for our map and show the impacted countires/states as points

--- src/components/CoronaMap.js
+++ src/components/CoronaMap.js
@@ -8,26 +8,84 @@ import '../styles/App.scss';
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOXGL_ACCESS_TOKEN;
 
 const CoronaMap = () => {
-    const mapboxElRef = useRef(null); // DOM element to render map
+    const mapboxRef = useRef(null); // DOM element to render map
+
+    // Conversion of fetched data to mapbox geojson formatted data
+    const fetcher = url =>
+        fetch(url)
+            .then(r => r.json())
+            .then(data =>
+                data.map((point, index) => ({
+                    type: "Feature",
+                    geometry: {
+                        type: "Point",
+                        coordinates: [
+                            point.coordinates.longitude,
+                            point.coordinates.latitude
+                        ]
+                    },
+                    properties: {
+                        id: index,
+                        country: point.country,
+                        province: point.province,
+                        cases: point.stats.confirmed,
+                        deaths: point.stats.deaths
+                    }
+                }))
+            );
+
+    // Use swr from Zeit to fetch data from API
+    const { data } = useSWR("https://corona.lmao.ninja/v2/jhucsse", fetcher);
+
+    console.log('Fetched and converetd data: ', data);
 
     // Initialize the map
     useEffect(() => {
-        // You can store the map instance with useRef too
-        const map = new mapboxgl.Map({
-            container: mapboxElRef.current,
-            style: "mapbox://styles/mapbox/dark-v10",   // theme of the map
-            center: [76.33643, 22.54930],   // initial geo location
-            zoom: 4     // initial zoom
-        });
+        if (data) {
+            // You can store the map instance with useRef too
+            const map = new mapboxgl.Map({
+                container: mapboxRef.current,
+                style: "mapbox://styles/mapbox/dark-v10",   // theme of the map
+                center: [76.33643, 22.54930],   // initial geo location
+                zoom: 4     // initial zoom
+            });
 
-        // Add navigation controls to the top right of the canvas
-        map.addControl(new mapboxgl.NavigationControl());
-    }, []);
+            // Add navigation controls to the top right of the canvas
+            map.addControl(new mapboxgl.NavigationControl());
+
+            // Call this method when the map is loaded
+            map.once("load", () => {
+                // Add our SOURCE
+                // with id "points"
+                map.addSource("points", {
+                    type: "geojson",
+                    data: {
+                        type: "FeatureCollection",
+                        features: data
+                    }
+                });
+
+                // Add our layer
+                map.addLayer({
+                    id: "circles",
+                    source: "points", // this should be the id of the source
+                    type: "circle",
+                    // paint properties
+                    paint: {
+                        "circle-opacity": 0.75,
+                        "circle-stroke-width": 1,
+                        "circle-radius": 4,
+                        "circle-color": "#FF4500"
+                    }
+                });
+            });
+        }
+    }, [data]);
 
     return(
         <div className="mapContainer">
             {/* Assigned Mapbox container */}
-            <div className="mapBox" ref={mapboxElRef} />
+            <div className="mapBox" ref={mapboxRef} />
         </div>
     );
 }
