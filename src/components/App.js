a11457ff9621974495394d4bfc81576e5df05cb2
Pulak Chakraborty <pulakchakraborty.contact@gmail.com>
2020-05-08 11:33:55 +0200

    add a side panel component - basic styling and component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import SidePanel from './SidePanel';
 import CoronaMap from './CoronaMap';
 import '../styles/App.scss';
 
@@ -7,6 +8,7 @@ const App = () => {
     return(
         <div className="App">
             <CoronaMap />
+            <SidePanel />
         </div>
     );
 }
---
+++ src/components/SidePanel.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../styles/SidePanel.scss';
+
+const SidePanel = () => {
+    return(
+        <div className="side-panel">
+            Side Panel Component
+        </div>
+    );
+}
+
+export default SidePanel;
