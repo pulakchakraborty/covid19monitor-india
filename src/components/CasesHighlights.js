260c564c96b9c57d427803c98450c368bfe50e26
Pulak Chakraborty <pulakchakraborty.contact@gmail.com>
2020-05-10 16:01:10 +0200

    fecth data from api and populate stats highlights

--- src/components/CasesHighlights.js
+++ src/components/CasesHighlights.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { getCurrentTimestamp } from '../helpers';
 import '../styles/CasesHighlights.scss';
 
-const CasesHighlights = () => {
+const CasesHighlights = (props) => {
+    const { total, discharged, deaths } = props.summary;
     return(
         <div className="cases-highlights-wrapper">
             <div className="cases-highlights-header">
@@ -13,15 +14,15 @@ const CasesHighlights = () => {
             </div>
             <div className="cases-highlights-stats">
                 <div className="highlights-stats-confirmed">
-                    <div className="highlights-stats-number">1000000</div>
+                    <div className="highlights-stats-number">{total}</div>
                     <div className="highlights-stats-type">confirmed</div>
                 </div>
                 <div className="highlights-stats-recovered">
-                    <div className="highlights-stats-number">500000</div>
+                    <div className="highlights-stats-number">{discharged}</div>
                     <div className="highlights-stats-type">recovered</div>
                 </div>
                 <div className="highlights-stats-dead">
-                    <div className="highlights-stats-number">100000</div>
+                    <div className="highlights-stats-number">{deaths}</div>
                     <div className="highlights-stats-type">dead</div>
                 </div>
             </div>
--- src/components/SidePanel.js
+++ src/components/SidePanel.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import useSWR from 'swr';
 
 import '../styles/SidePanel.scss';
 import CasesHighlights from './CasesHighlights';
+import config from '../config';
 
 const SidePanel = () => {
+    const [ indiaCases, setIndiaCases ] = useState({});
+    const [ summary, setSummery ] = useState({ total: 0, deaths: 0, discharged: 0 });
+    const { indiaLatest } = config;
+    const fetcher = async (url) =>
+    await fetch(url)
+        .then(r => r.json())
+        .then(response => setIndiaCases(response.data));
+
+    // Use swr from Zeit to fetch data from API
+    const { data, error } = useSWR(indiaLatest, fetcher);
+    useEffect(() => {
+        if(indiaCases.summary) {
+            setSummery(indiaCases.summary);
+            console.log("inside UE: ", summary);
+        }
+    }, [indiaCases, summary]);
+
     return(
         <div className="side-panel">
-            <CasesHighlights />
+            <CasesHighlights summary={summary} />
         </div>
     );
+
 }
 
 export default SidePanel;
