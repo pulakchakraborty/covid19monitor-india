d26a2e7c8716a005924ea00f961356ba21e6e239
Pulak Chakraborty <pulakchakraborty.contact@gmail.com>
2020-05-11 19:58:39 +0200

    populate table from regional data

--- src/components/CasesTable.js
+++ src/components/CasesTable.js
@@ -1,12 +1,55 @@
 import React from 'react';
+import { useTable, useSortBy } from 'react-table';
 
 import '../styles/CasesTable.scss';
 
-const CasesTable = () => {
-    return(
-        <div className="cases-table">
-            Table Component
-        </div>
+const CasesTable = ({ columns, data }) => {
+    // Use the useTable Hook to send the columns and data to build the table
+    const {
+        getTableProps,      // table props from react-table
+        getTableBodyProps,  // table body props from react-table
+        headerGroups,       // headerGroups, if your table has groupings
+        rows,               // rows for the table based on the data passed
+        prepareRow          // prepare the row (this function needs to be called for each row before getting the row props)
+    } = useTable({
+            columns,
+            data
+        },
+        useSortBy           // hook for getting sorting capability
+    );
+
+    console.log("CasesTable Component: ", columns);
+
+    /*
+    Render the UI for your table
+    - react-table doesn't have UI, it's headless. We just need to put the react-table props from the Hooks
+    */
+    return (
+        <table className="cases-table" {...getTableProps()}>
+            <thead className="cases-table-head">
+                {headerGroups.map(headerGroup => (
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                        {headerGroup.headers.map(column => (
+                            <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                                {column.render("Header")}
+                            </th>
+                        ))}
+                    </tr>
+                ))}
+            </thead>
+            <tbody {...getTableBodyProps()}>
+                {rows.map((row, i) => {
+                    prepareRow(row);
+                    return (
+                        <tr {...row.getRowProps()}>
+                            {row.cells.map(cell => {
+                                return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
+                            })}
+                        </tr>
+                    );
+                })}
+            </tbody>
+        </table>
     );
 };
 
--- src/components/SidePanel.js
+++ src/components/SidePanel.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import useSWR from 'swr';
 import axios from 'axios';
 
@@ -6,13 +6,17 @@ import '../styles/SidePanel.scss';
 import CasesHighlights from './CasesHighlights';
 import CasesTable from './CasesTable';
 import config from '../config';
+import { TableSettings } from '../config/TableSettings';
 
 const SidePanel = () => {
     const [ indiaCases, setIndiaCases ] = useState({});
     const [ summary, setSummary ] = useState({ total: 0, deaths: 0, discharged: 0 });
+    const [ tableData, setTableData ] = useState([]);
     const [ errorMessage, setErrorMessage ] = useState('');
     const [ hasError, setHasError ] = useState(false);
     const { indiaLatest } = config;
+
+    const columns = useMemo(() => [{...TableSettings}], []);
 
     useEffect(() => {
         (async () => {
@@ -21,6 +25,7 @@ const SidePanel = () => {
                 if (status === 200) {
                     setIndiaCases(response.data);
                     setSummary(response.data.summary);
+                    setTableData(response.data.regional);
                 }
             } catch(e) {
                 if (e.response) {
@@ -34,7 +39,7 @@ const SidePanel = () => {
     return(
         <div className="side-panel">
             <CasesHighlights summary={summary} />
-            <CasesTable />
+            <CasesTable columns={columns} data={tableData} />
         </div>
     );
 
