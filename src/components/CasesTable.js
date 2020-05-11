db7790417ec602f9a0f80fea94f39191882ea50a
Pulak Chakraborty <pulakchakraborty.contact@gmail.com>
2020-05-11 17:15:23 +0200

    use axios to fetch data - fetch data inside useEffect hook - error handling

---
+++ src/components/CasesTable.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../styles/CasesTable.scss';
+
+const CasesTable = () => {
+    return(
+        <div className="cases-table">
+            Table Component
+        </div>
+    );
+};
+
+export default CasesTable;
--- src/components/SidePanel.js
+++ src/components/SidePanel.js
@@ -1,31 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import useSWR from 'swr';
+import axios from 'axios';
 
 import '../styles/SidePanel.scss';
 import CasesHighlights from './CasesHighlights';
+import CasesTable from './CasesTable';
 import config from '../config';
 
 const SidePanel = () => {
     const [ indiaCases, setIndiaCases ] = useState({});
-    const [ summary, setSummery ] = useState({ total: 0, deaths: 0, discharged: 0 });
+    const [ summary, setSummary ] = useState({ total: 0, deaths: 0, discharged: 0 });
+    const [ errorMessage, setErrorMessage ] = useState('');
+    const [ hasError, setHasError ] = useState(false);
     const { indiaLatest } = config;
-    const fetcher = async (url) =>
-    await fetch(url)
-        .then(r => r.json())
-        .then(response => setIndiaCases(response.data));
 
-    // Use swr from Zeit to fetch data from API
-    const { data, error } = useSWR(indiaLatest, fetcher);
     useEffect(() => {
-        if(indiaCases.summary) {
-            setSummery(indiaCases.summary);
-            console.log("inside UE: ", summary);
-        }
-    }, [indiaCases, summary]);
+        (async () => {
+            try {
+                const { data: response, status } = await axios.get(indiaLatest);
+                if (status === 200) {
+                    setIndiaCases(response.data);
+                    setSummary(response.data.summary);
+                }
+            } catch(e) {
+                if (e.response) {
+                    setHasError(true);
+                    setErrorMessage(e.response.data.message);
+                }
+            }
+          })();
+    }, []);
 
     return(
         <div className="side-panel">
             <CasesHighlights summary={summary} />
+            <CasesTable />
         </div>
     );
 
