49389be5f49887db3ad9618152953a6f82071135
Pulak Chakraborty <pulakchakraborty.contact@gmail.com>
2020-05-12 22:45:19 +0200

    use recharts to draw stacked area charts - replace react-vis by recharts

--- src/components/CasesChart.js
+++ src/components/CasesChart.js
@@ -1,52 +1,52 @@
 import React from 'react';
-import {XYPlot, XAxis, YAxis, VerticalGridLines, HorizontalGridLines, LineSeries} from 'react-vis';
+import { AreaChart, Area, XAxis, YAxis, Tooltip, ResponsiveContainer, Legend } from 'recharts';
+import styled from 'styled-components';
+
+const Styles = styled.div`
+    padding: 1rem;
+    width: 95%;
+    height: 200px;
+    .recharts-cartesian-axis-ticks {
+        font-size: 0.7rem;
+    }
+    .recharts-legend-item-text {
+        font-size: 0.9rem;
+    }
+`
 
 const CasesChart = ({ chartData }) => {
-    //console.log("chart data: ", chartData);
-    /*const dataArr = chartData.map((d)=> {
-        return {x: d.day,
-        y: parseFloat(d.summary.total/1000)}
-    });*/
     const dataArr = chartData.map((data, index) => {
-        return {x: index,
-        y: data.summary.total}
+        return {index: data.day,
+            confirmed: data.summary.total,
+            recovered: data.summary.discharged,
+            dead: data.summary.deaths
+        }
     });
 
-    const axisStyle = {
-        line: {
-            stroke: '#696969'
-        },
-        ticks: {
-            fontSize: '0.6rem',
-            fill: '#696969'
-        },
-        title: {
-            fill: '#696969',
-            fontSize: '0.8rem'
-        }
-      };
-
-    console.log("data arr:",dataArr);
-
-    return (
-        <XYPlot width={400} height={200} >
-            <VerticalGridLines />
-            <HorizontalGridLines />
-            <XAxis
-                tickValues={[0, 30, 60]}
-                tickFormat={v => chartData[v].day}
-                tickSize={3}
-                style={axisStyle} />
-            <YAxis
-                tickValues={dataArr.y}
-                tickFormat={v => `${Math.round(v/1000)}K`}
-                tickPadding={-30}
-                tickSize={3}
-                style={axisStyle} />
-            <LineSeries
-                data={dataArr}
-                style={{stroke: 'red', strokeWidth: 1}} />
-        </XYPlot>
+    return(
+        <Styles>
+            <ResponsiveContainer>
+                <AreaChart
+                    data={dataArr}
+                    margin={{
+                    top: 10, right: 20, left: 10, bottom: 0,
+                    }}
+                >
+                    <XAxis dataKey="index" />
+                    <YAxis
+                        type="number"
+                        tickCount="2"
+                        tickSize="6"
+                        mirror="true"
+                        domain={[0, 'dataMax']} />
+                    <Tooltip />
+                    <Legend />
+                    <Area type="monotone" dataKey="confirmed" stackId="1" stroke="#d14f69" fill="rgba(209,79,105,0.2)" />
+                    <Area type="monotone" dataKey="recovered" stackId="2" stroke="#108d90" fill="rgba(129,255,202,0.3)" />
+                    <Area type="monotone" dataKey="dead" stackId="3" stroke="rgb(63, 59, 59)" fill="grey" />
+                </AreaChart>
+            </ResponsiveContainer>
+        </Styles>
     );
 };
 
