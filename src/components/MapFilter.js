eacfdfcdee622b0c77528a2e5aa374994df435bd
Pulak Chakraborty <pulakchakraborty.contact@gmail.com>
2020-05-18 12:53:38 +0200

    hover and focus on map selection buttons

--- src/components/MapFilter.js
+++ src/components/MapFilter.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import styled, { css } from 'styled-components';
 
 const Button = styled.button`
-background: transparent;
+    background: transparent;
     border-radius: 4px;
     border: 2px solid #d14f69;
     color: #d14f69;
@@ -16,6 +16,21 @@ background: transparent;
     opacity: 0.5;
     cursor: pointer;
     pointer-events: all;
+
+    /* "down" effect on click */
+    &:active {
+        transform: translateY(1px);
+        filter: saturate(150%);
+    }
+
+    /* inverse colors on mouse-over */
+    &:hover,
+    &:focus {
+        color: #e63c5e;
+        border-color: #e63c5e;
+        background: rgba(209,79,105,0.2);
+        opacity: 1;
+    }
 
     ${props => props.isActive && css`
         background: rgba(209,79,105,0.2);
